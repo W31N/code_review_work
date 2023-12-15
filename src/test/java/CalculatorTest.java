e26a36a5e7bc6ab4f2e86fb54fa4213c025b479c
Ivanov Ivan <timafolf2003@mail.ru>
2023-12-15 10:20:19 +0300

    change README.md, add methods for calculator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,26 +6,44 @@ class CalculatorTest {
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        Calculator calculator = new Calculator();
+        int result = calculator.add(-6, 5);
+        assertEquals(-1, result);
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        Calculator calculator = new Calculator();
+        int result = calculator.dif(0, 5);
+        assertEquals(-5, result);
+    }
+
+    @Test
+    void divByZero() {
+        Calculator calculator = new Calculator();
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.div(10, 0);
+        });
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        Calculator calculator = new Calculator();
+        int result = calculator.div(18, 6);
+        assertEquals(3, result);
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        Calculator calculator = new Calculator();
+        int result = calculator.times(5, 6);
+        assertEquals(30, result);
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        Calculator calculator = new Calculator();
+        int result = calculator.solver(5, 8, 3);
+        assertEquals(143, result);
     }
 }
